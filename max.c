cf368270e44fe9b1a7c4a66c137e6dc722075f67
tgowri <gowrithulasiraj@gmail.com>
2018-09-03 12:42:03 +0530

    Create max.c

---
+++ max.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+3include<conio.h>
+void main()
+{
+int n,a[10],i,max;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+max=a[0];
+for(i=0;i<n;i++)
+{
+if(max>a[0])
+{
+max=a[0];
+}
+}
+printf("%d",max);
+getch();
+}
